0f443a4767ea6699e2867db53cdee40e1b8f0b97
Alex Dunphy <alexanderdunphy@gmail.com>
2012-04-18 17:23:58 +0100

    First commit
    Added files, created readme

---
+++ jquery.flexText.js
@@ -0,0 +1,77 @@
+/**
+ * jQuery flexText: Auto-height textareas
+ * --------------------------------------
+ * Requires: jQuery 1.7+
+ * Usage example: $('textarea').flexText()
+ * Info: https://github.com/alexdunphy/flexible-textareas
+ */
+;(function ($) {
+
+    // Constructor
+    function FT(elem) {
+        this.$textarea = $(elem);
+
+        this._init();
+    }
+
+    FT.prototype = {
+        _init: function () {
+            var _this = this;
+
+            // Insert wrapper elem & pre/span for textarea mirroring
+            this.$textarea.wrap('<div class="flex-text-wrap" />').before('<pre><span /><br /><br /></pre>');
+
+            this.$span = this.$textarea.prev().find('span');
+
+            // Add input event listeners
+            this.$textarea.on('input propertychange', function () {
+                _this._mirror();
+            });
+
+            // IE polyfills...
+            if ($.browser.msie) {
+                /*
+                 * IE 9 & 10 have buggy input event support (firing only on char addition, not char removal)
+                 * Binding to keyup catches keyboard-triggered undos/cuts/deletes
+                 * Binding to change catches mouse-triggered undos/cuts/deletions (when textarea loses focus)
+                 */
+                if ($.browser.version > 8) {
+                    this.$textarea.on('keyup change', function () {
+                        _this._mirror();
+                    });
+                } else if ($.browser.version < 8) {
+                    /**
+                     * jQuery val() strips carriage return chars by default (see http://api.jquery.com/val/)
+                     * This causes issues in IE7, but a valHook can be used to preserve these chars
+                     */
+                    $.valHooks.textarea = {
+                        get: function (elem) {
+                            return elem.value.replace(/\r?\n/g, "\r\n");
+                        }
+                    };
+                }
+            }
+
+
+            // Mirror contents once on init
+            this._mirror();
+        }
+
+        // Mirror pre/span & textarea contents
+        ,_mirror: function () {
+            this.$span.text(this.$textarea.val());
+        }
+    };
+
+    // jQuery plugin wrapper
+    $.fn.flexText = function () {
+        return this.each(function () {
+            // Check if already instantiated on this elem
+            if (!$.data(this, 'flexText')) {
+                // Instantiate & store elem + string
+                $.data(this, 'flexText', new FT(this));
+            }
+        });
+    };
+
+})(jQuery);
